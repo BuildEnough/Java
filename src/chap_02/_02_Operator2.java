5e9d42b626f15b867baa2edbfb74a78444a4533c
BuildEnough <rhtmaehcl878@naver.com>
2022-12-26 21:00:18 +0900

    대입 연산자/비교 연산자/논리 연산자/삼항 연산자

--- src/chap_02/_02_Operator2.java
+++ src/chap_02/_02_Operator2.java
@@ -21,6 +21,27 @@ public class _02_Operator2 {
 
         // 복합 대입 연산자
         num = 10;
+        // num = num + 2;
+        num += 2;
+        System.out.println(num); // 12
+
+        // num  = num - 2;
+        num -= 2;
+        System.out.println(num); // 10
+
+        // num = num * 2;
+        num *= 2;
+        System.out.println(num); // 20
+
+        // num = num / 2;
+        num /= 2;
+        System.out.println(num); // 10
+
+        // num = num % 2;
+        num %= 2;
+        System.out.println(num); // 0
+
+
 
     }
 }
