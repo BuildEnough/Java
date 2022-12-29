b726f82e46af036633b70bed38cb9c2309388dc3
BuildEnough <rhtmaehcl878@naver.com>
2022-12-29 09:27:07 +0900

    Quiz 4

--- src/chap_04/_06_while.java
+++ src/chap_04/_06_while.java
@@ -6,7 +6,7 @@ public class _06_while {
         // 수영장에서 수영 하는 모습
         int distance = 25; // 전체 거리 25m
         int move = 0; // 현재 이동 거리 0m
-        while (move < distance) { // 현재 이동거리가 전체 거리보다 작다는 조건이 참인 동나 반복 수행
+        while (move < distance) { // 현재 이동거리가 전체 거리보다 작다는 조건이 참인 동안 반복 수행
             System.out.println("발차기를 계속 합니다.");
             System.out.println("현재 이동 거리: " + move);
             move += 3; // 3미터 이동
@@ -15,7 +15,7 @@ public class _06_while {
 
         // 무한 루프
         move = 0;
-        while (move < distance) { // 현재 이동거리가 전체 거리보다 작다는 조건이 참인 동나 반복 수행
+        while (move < distance) { // 현재 이동거리가 전체 거리보다 작다는 조건이 참인 동안 반복 수행
             System.out.println("발차기를 계속 합니다.");
             System.out.println("현재 이동 거리: " + move);
         }
