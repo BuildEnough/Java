84ad103423eb5397a6f06f5b92ab96a130e7278e
BuildEnough <rhtmaehcl878@naver.com>
2025-05-12 23:45:58 +0900

    자바(객체지향 프로그래밍)

---
+++ java-basic/src/oop1/MusicPlayerMain4.java
@@ -0,0 +1,30 @@
+package oop1;
+
+import static oop1.MusicPalyerMain2.volumeDown;
+import static oop1.MusicPalyerMain2.volumeUp;
+
+public class MusicPlayerMain4 {
+    public static void main(String[] args) {
+        MusicPlayer player = new MusicPlayer();
+
+        //음악 플레이어 켜기
+        player.on();
+
+        // 볼륨 증가
+        player.volumeUp();
+
+        // 볼륨 증가
+        player.volumeUp();
+
+        // 볼륨 감소
+        player.volumeDown();
+
+
+        // 음악 플레이어 상태
+        player.showStatus();
+
+        // 음악 플레이어 끄기
+        player.off();
+
+    }
+}
