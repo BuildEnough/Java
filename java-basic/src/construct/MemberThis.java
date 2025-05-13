4cd51c1c2e9e4c37c74866a9493d6df0823f520f
BuildEnough <rhtmaehcl878@naver.com>
2025-05-13 23:16:58 +0900

    자바(생성자)

---
+++ java-basic/src/construct/MemberThis.java
@@ -0,0 +1,10 @@
+package construct;
+
+public class MemberThis {
+    String nameField;
+
+    void initMember(String nameParameter, String nameField) {
+        this.nameField = nameParameter;
+        nameField = nameParameter;
+    }
+}
